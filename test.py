ed75f82b82a05c73b060de85dc1a3055da5734cc
SNURFER <shido322@snu.ac.kr>
2021-08-20 23:13:49 +0900

    1. add today data 2. add menu string for every meal 3. modified row column of menuTable 4. check saturday, sunday

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-import selenium, os, shutil, time, pandas, json
+import selenium, os, shutil, time, pandas, json, datetime
 from selenium import webdriver
 from selenium.webdriver import ActionChains
 
@@ -66,7 +66,6 @@ def parseXLSX(filePath):
     menuSheet = pandas.read_excel(filePath, sheet_name=0, engine='openpyxl')
 
     totalRow = menuSheet.shape[0] - 1
-    # print(menuSheet)
 
     bldIdx = []
     menuIdx = []
@@ -77,15 +76,9 @@ def parseXLSX(filePath):
         if (pandas.isnull(row[2]) == False):
             menuIdx.append(row[0])
             
-    # print('breakfast lunch dinner start idx')
-    # print(bldIdx)
-    # print('---------')
-    # print('menu start idx')
-    # print(menuIdx)
-
     # mon tue wed thu fri
     # breakfast lunch dinner
-    menuTable = [[0 for i in range(5)] for j in range(3)]
+    menuTable = [[0 for i in range(3)] for j in range(5)]
     for i in range(0, 5): 
         bStr = ""
         lStr = "" 
@@ -93,50 +86,64 @@ def parseXLSX(filePath):
         for row in menuSheet.itertuples():
             #breakfast
             if (bldIdx[1] <= row[0] and row[0] < bldIdx[2] and pandas.isnull(row[i + 3]) == False):
+                if (row[0] in menuIdx):
+                    bStr += '\n' + row[2] + '\n'
                 bStr += row[i + 3] + '\n'
             #lunch
             if (bldIdx[2] <= row[0] and row[0] < bldIdx[3] and pandas.isnull(row[i + 3]) == False):
+                if (row[0] in menuIdx):
+                    lStr += '\n' + row[2] + '\n'
                 lStr += row[i + 3] + '\n'
             #dinner
             if (bldIdx[3] <= row[0] and row[0] < totalRow and pandas.isnull(row[i + 3]) == False):
+                if (row[0] in menuIdx):
+                    dStr += '\n' + row[2] + '\n'
                 dStr += row[i + 3] + '\n'
-        menuTable[0][i] = bStr
-        menuTable[1][i] = lStr
-        menuTable[2][i] = dStr
 
-    print('friday print test')
-    print('br')
-    print(menuTable[0][3])
-    print('lu')
-    print(menuTable[1][3])
-    print('di')
-    print(menuTable[2][3])
+        menuTable[i][0] = bStr
+        menuTable[i][1] = lStr
+        menuTable[i][2] = dStr
+
+    dt = datetime.datetime.now()
+    todayBreakfast = ''
+    todayLunch = '' 
+    todayDinner = ''
+    if (dt.strftime('%A') != 'Saturday' and dt.strftime('%A') != 'Sunday'):
+        weekday = datetime.datetime.today().weekday()
+        todayBreakfast = menuTable[weekday][0]
+        todayLunch = menuTable[weekday][1]
+        todayDinner = menuTable[weekday][2]
  
     menuJson = {
         "monday" : { 
             "breakfast" : menuTable[0][0],    
-            "lunch" : menuTable[1][0],    
-            "dinner" : menuTable[2][0],    
+            "lunch" : menuTable[0][1],    
+            "dinner" : menuTable[0][2],    
         }, 
         "tuesday" : {
-            "breakfast" : menuTable[0][1],    
+            "breakfast" : menuTable[1][0],    
             "lunch" : menuTable[1][1],    
-            "dinner" : menuTable[2][1],   
+            "dinner" : menuTable[1][2],   
         },
         "wednesday" : {
-            "breakfast" : menuTable[0][2],   
-            "lunch" : menuTable[1][2],   
+            "breakfast" : menuTable[2][0],   
+            "lunch" : menuTable[2][1],   
             "dinner" : menuTable[2][2],  
         },
         "thursday" : {
-            "breakfast" : menuTable[0][3],  
-            "lunch" : menuTable[1][3],    
-            "dinner" : menuTable[2][3],   
+            "breakfast" : menuTable[3][0],  
+            "lunch" : menuTable[3][1],    
+            "dinner" : menuTable[3][2],   
         },
         "friday" : {
-            "breakfast" : menuTable[0][4],   
-            "lunch" : menuTable[1][4],   
-            "dinner" : menuTable[2][4],    
+            "breakfast" : menuTable[4][0],   
+            "lunch" : menuTable[4][1],   
+            "dinner" : menuTable[4][2],    
+        },
+        "today" : {
+            "breakfast" : todayBreakfast,   
+            "lunch" : todayLunch,   
+            "dinner" : todayDinner,    
         }
     }
 
