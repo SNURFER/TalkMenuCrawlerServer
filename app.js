2a7656820f3766e2043c532be9e9139b14afc343
SNURFER <shido322@snu.ac.kr>
2021-08-24 23:04:06 +0900

    insert skill message handling

--- app.js
+++ app.js
@@ -24,64 +24,201 @@ const job = schedule.scheduleJob('30 24 * * 6,7', () => {
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.get('/keyboard', (req, res) => {
+app.post('/message', function (req, res) {
     const fs = require('fs');
-
     const jsonFile = fs.readFileSync('./menu.json', 'utf8');
-    console.log(jsonFile);
     const data = JSON.parse(jsonFile);
 
-    res.json(data);
-});
+    const question = req.body.userRequest.utterance;
+    let responseText = '';
+    const quickReplies = [];
 
-app.get('/monday', (req, res) => {
-    const fs = require('fs');
+    if (question == "오늘" || question == "메뉴") {
+        responseText = "시간을 선택하세요";
+        quickReplies.push({
+            'label': '아침',
+            'action': 'message',
+            'messageText': '아침'
+        });
+        quickReplies.push({
+            'label': '점심',
+            'action': 'message',
+            'messageText': '점심'
+        });
+        quickReplies.push({
+            'label': '저녁',
+            'action': 'message',
+            'messageText': '저녁'
+        });
+    }
 
-    const jsonFile = fs.readFileSync('./menu.json', 'utf8');
-    console.log(jsonFile);
-    const data = JSON.parse(jsonFile);
+    if (question == "아침") {
+        responseText = data["today"]["breakfast"];
+    }
+    if (question == "점심") {
+        responseText = data["today"]["lunch"];
+    }
+    if (question == "저녁") {
+        responseText = data["today"]["lunch"];
+    }
 
-    res.json(data["monday"]);
-});
+    if (question == "월요일") {
+        responseText = "시간을 선택하세요";
+        quickReplies.push({
+            'label': '아침',
+            'action': 'message',
+            'messageText': '월요일 아침'
+        });
+        quickReplies.push({
+            'label': '점심',
+            'action': 'message',
+            'messageText': '월요일 점심'
+        });
+        quickReplies.push({
+            'label': '저녁',
+            'action': 'message',
+            'messageText': '월요일 저녁'
+        });
+    }
+    if (question == "월요일 아침") {
+        responseText = data["monday"]["breakfast"];
+    }
+    if (question == "월요일 점심") {
+        responseText = data["monday"]["lunch"];
+    }
+    if (question == "월요일 저녁") {
+        responseText = data["monday"]["dinner"];
+    }
 
-app.get('/tuesday', (req, res) => {
-    const fs = require('fs');
 
-    const jsonFile = fs.readFileSync('./menu.json', 'utf8');
-    console.log(jsonFile);
-    const data = JSON.parse(jsonFile);
+    if (question == "화요일") {
+        responseText = "시간을 선택하세요";
+        quickReplies.push({
+            'label': '아침',
+            'action': 'message',
+            'messageText': '화요일 아침'
+        });
+        quickReplies.push({
+            'label': '점심',
+            'action': 'message',
+            'messageText': '화요일 점심'
+        });
+        quickReplies.push({
+            'label': '저녁',
+            'action': 'message',
+            'messageText': '화요일 저녁'
+        });
+    }
+    if (question == "화요일 아침") {
+        responseText = data["tuesday"]["breakfast"];
+    }
+    if (question == "화요일 점심") {
+        responseText = data["tuesday"]["lunch"];
+    }
+    if (question == "화요일 저녁") {
+        responseText = data["tuesday"]["dinner"];
+    }
 
-    res.json(data["tuesday"]);
-});
 
-app.get('/wednesday', (req, res) => {
-    const fs = require('fs');
+    if (question == "수요일") {
+        responseText = "시간을 선택하세요";
+        quickReplies.push({
+            'label': '아침',
+            'action': 'message',
+            'messageText': '수요일 아침'
+        });
+        quickReplies.push({
+            'label': '점심',
+            'action': 'message',
+            'messageText': '수요일 점심'
+        });
+        quickReplies.push({
+            'label': '저녁',
+            'action': 'message',
+            'messageText': '수요일 저녁'
+        });
+    }
+    if (question == "수요일 아침") {
+        responseText = data["wednesday"]["breakfast"];
+    }
+    if (question == "수요일 점심") {
+        responseText = data["wednesday"]["lunch"];
+    }
+    if (question == "수요일 저녁") {
+        responseText = data["wednesday"]["dinner"];
+    }
 
-    const jsonFile = fs.readFileSync('./menu.json', 'utf8');
-    console.log(jsonFile);
-    const data = JSON.parse(jsonFile);
+    if (question == "목요일") {
+        responseText = "시간을 선택하세요";
+        quickReplies.push({
+            'label': '아침',
+            'action': 'message',
+            'messageText': '목요일 아침'
+        });
+        quickReplies.push({
+            'label': '점심',
+            'action': 'message',
+            'messageText': '목요일 점심'
+        });
+        quickReplies.push({
+            'label': '저녁',
+            'action': 'message',
+            'messageText': '목요일 저녁'
+        });
+    }
+    if (question == "목요일 아침") {
+        responseText = data["thursday"]["breakfast"];
+    }
+    if (question == "목요일 점심") {
+        responseText = data["thursday"]["lunch"];
+    }
+    if (question == "목요일 저녁") {
+        responseText = data["thursday"]["dinner"];
+    }
 
-    res.json(data["wednesday"]);
-});
+    if (question == "금요일") {
+        responseText = "시간을 선택하세요";
+        quickReplies.push({
+            'label': '아침',
+            'action': 'message',
+            'messageText': '금요일 아침'
+        });
+        quickReplies.push({
+            'label': '점심',
+            'action': 'message',
+            'messageText': '금요일 점심'
+        });
+        quickReplies.push({
+            'label': '저녁',
+            'action': 'message',
+            'messageText': '금요일 저녁'
+        });
+    }
+    if (question == "금요일 아침") {
+        responseText = data["friday"]["breakfast"];
+    }
+    if (question == "금요일 점심") {
+        responseText = data["friday"]["lunch"];
+    }
+    if (question == "금요일 저녁") {
+        responseText = data["friday"]["dinner"];
+    }
 
-app.get('/thursday', (req, res) => {
-    const fs = require('fs');
+    const responseBody = {
+        'version': '2.0',
+        'template': {
+            'outputs': [
+                {
+                    'simpleText': {
+                        'text': responseText 
+                    }
+                }
+            ],
+            'quickReplies': quickReplies
+        }
+    };
 
-    const jsonFile = fs.readFileSync('./menu.json', 'utf8');
-    console.log(jsonFile);
-    const data = JSON.parse(jsonFile);
-
-    res.json(data["thursday"]);
-});
-
-app.get('/friday', (req, res) => {
-    const fs = require('fs');
-
-    const jsonFile = fs.readFileSync('./menu.json', 'utf8');
-    console.log(jsonFile);
-    const data = JSON.parse(jsonFile);
-
-    res.json(data["friday"]);
+    res.status(200).send(responseBody);
 });
 
 app.listen(3000, () => console.log('node on 3000'));
