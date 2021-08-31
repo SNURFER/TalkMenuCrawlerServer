43bcc19b843c84dd3f978adde326cb7420d3f134
SNURFER <shido322@snu.ac.kr>
2021-09-01 00:37:36 +0900

    [refactor] remove code duplication - make functions to reduce code duplication

--- app.js
+++ app.js
@@ -5,6 +5,25 @@ const path = require('path');
 const express = require('express');
 
 const app = express();
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+const fs = require('fs');
+
+let data;
+
+const translator = {
+    '월요일': 'monday',
+    '화요일': 'tuesday',
+    '수요일': 'wednesday',
+    '목요일': 'thursday',
+    '금요일': 'friday',
+    '오늘': 'today',
+    '아침': 'breakfast',
+    '점심': 'lunch',
+    '저녁': 'dinner',
+    '메뉴': 'today',
+};
 
 function getWeekDayString(num) {
     if (num == 1) return 'monday';
@@ -14,6 +33,16 @@ function getWeekDayString(num) {
     if (num == 5) return 'friday';
 
     return 'friday';
+}
+
+function isDay(str) {
+    if (str === '월요일' || str === '화요일' || str === '수요일' || str === '목요일' || str === '금요일' || str === '오늘') return true;
+    return false;
+}
+
+function isMeal(str) {
+    if (str === '아침' || str === '점심' || str === '저녁') return true;
+    return false;
 }
 
 function runPyScript(){
@@ -31,191 +60,76 @@ const job = schedule.scheduleJob('30 24 * * 6,7', () => {
     console.log('매주 토, 일 자정에 수행');
 });
 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-
-app.post('/message', function (req, res) {
-    const fs = require('fs');
+const dailyJob = schedule.scheduleJob('1 0 * * *', () => {
+    console.log('매일 자정에 메뉴를 파싱')
     const jsonFile = fs.readFileSync('./menu.json', 'utf8');
-    const data = JSON.parse(jsonFile);
+    data = JSON.parse(jsonFile);
 
     const date = new Date();
     const dayNum = date.getDay();
     const dayStr = getWeekDayString(dayNum);
 
-    const question = req.body.userRequest.utterance;
+    data["today"] = data[dayStr];
+});
+
+app.post('/message', function (req, res) {
+
+    const utterance = req.body.userRequest.utterance;
     let responseText = '';
     const quickReplies = [];
 
-    if (question == "오늘" || question == "메뉴") {
-        responseText = "시간을 선택하세요";
-        quickReplies.push({
-            'label': '아침',
-            'action': 'message',
-            'messageText': '아침'
-        });
-        quickReplies.push({
-            'label': '점심',
-            'action': 'message',
-            'messageText': '점심'
-        });
-        quickReplies.push({
-            'label': '저녁',
-            'action': 'message',
-            'messageText': '저녁'
-        });
+    let utteranceArray = utterance.split(' ');
+    if (utteranceArray.length === 2) {
+        responseText = data[translator[utteranceArray[0]]][translator[utteranceArray[1]]];
+    }
+    else if (utteranceArray.length === 1) {
+        if (isDay(utteranceArray[0])) {
+            responseText = "시간을 선택하세요";
+            quickReplies.push({
+                'label': '아침',
+                'action': 'message',
+                'messageText':  utteranceArray[0] + ' 아침'
+            });
+            quickReplies.push({
+                'label': '점심',
+                'action': 'message',
+                'messageText': utteranceArray[0] + ' 점심'
+            });
+            quickReplies.push({
+                'label': '저녁',
+                'action': 'message',
+                'messageText': utteranceArray[0] + ' 저녁'
+            });
+        }
+        else {
+            //exception cases 
+            //just breakfast, lunch, dinner case for today
+            if (isMeal(utteranceArray[0]))
+                responseText = data['today'][translator[utteranceArray[0]]];
+            //menu handling
+            else {
+                responseText = "시간을 선택하세요";
+                quickReplies.push({
+                    'label': '아침',
+                    'action': 'message',
+                    'messageText': '아침'
+                });
+                quickReplies.push({
+                    'label': '점심',
+                    'action': 'message',
+                    'messageText': '점심'
+                });
+                quickReplies.push({
+                    'label': '저녁',
+                    'action': 'message',
+                    'messageText': '저녁'
+                });
+            }
+        }
     }
 
-    if (question == "아침" || question == "오늘 아침") {
-        responseText = data[dayStr]["breakfast"];
-    }
-    if (question == "점심" || question == "오늘 점심") {
-        responseText = data[dayStr]["lunch"];
-    }
-    if (question == "저녁" || question == "오늘 저녁") {
-        responseText = data[dayStr]["dinner"];
-    }
-
-    if (question == "월요일") {
-        responseText = "시간을 선택하세요";
-        quickReplies.push({
-            'label': '아침',
-            'action': 'message',
-            'messageText': '월요일 아침'
-        });
-        quickReplies.push({
-            'label': '점심',
-            'action': 'message',
-            'messageText': '월요일 점심'
-        });
-        quickReplies.push({
-            'label': '저녁',
-            'action': 'message',
-            'messageText': '월요일 저녁'
-        });
-    }
-    if (question == "월요일 아침") {
-        responseText = data["monday"]["breakfast"];
-    }
-    if (question == "월요일 점심") {
-        responseText = data["monday"]["lunch"];
-    }
-    if (question == "월요일 저녁") {
-        responseText = data["monday"]["dinner"];
-    }
-
-
-    if (question == "화요일") {
-        responseText = "시간을 선택하세요";
-        quickReplies.push({
-            'label': '아침',
-            'action': 'message',
-            'messageText': '화요일 아침'
-        });
-        quickReplies.push({
-            'label': '점심',
-            'action': 'message',
-            'messageText': '화요일 점심'
-        });
-        quickReplies.push({
-            'label': '저녁',
-            'action': 'message',
-            'messageText': '화요일 저녁'
-        });
-    }
-    if (question == "화요일 아침") {
-        responseText = data["tuesday"]["breakfast"];
-    }
-    if (question == "화요일 점심") {
-        responseText = data["tuesday"]["lunch"];
-    }
-    if (question == "화요일 저녁") {
-        responseText = data["tuesday"]["dinner"];
-    }
-
-
-    if (question == "수요일") {
-        responseText = "시간을 선택하세요";
-        quickReplies.push({
-            'label': '아침',
-            'action': 'message',
-            'messageText': '수요일 아침'
-        });
-        quickReplies.push({
-            'label': '점심',
-            'action': 'message',
-            'messageText': '수요일 점심'
-        });
-        quickReplies.push({
-            'label': '저녁',
-            'action': 'message',
-            'messageText': '수요일 저녁'
-        });
-    }
-    if (question == "수요일 아침") {
-        responseText = data["wednesday"]["breakfast"];
-    }
-    if (question == "수요일 점심") {
-        responseText = data["wednesday"]["lunch"];
-    }
-    if (question == "수요일 저녁") {
-        responseText = data["wednesday"]["dinner"];
-    }
-
-    if (question == "목요일") {
-        responseText = "시간을 선택하세요";
-        quickReplies.push({
-            'label': '아침',
-            'action': 'message',
-            'messageText': '목요일 아침'
-        });
-        quickReplies.push({
-            'label': '점심',
-            'action': 'message',
-            'messageText': '목요일 점심'
-        });
-        quickReplies.push({
-            'label': '저녁',
-            'action': 'message',
-            'messageText': '목요일 저녁'
-        });
-    }
-    if (question == "목요일 아침") {
-        responseText = data["thursday"]["breakfast"];
-    }
-    if (question == "목요일 점심") {
-        responseText = data["thursday"]["lunch"];
-    }
-    if (question == "목요일 저녁") {
-        responseText = data["thursday"]["dinner"];
-    }
-
-    if (question == "금요일") {
-        responseText = "시간을 선택하세요";
-        quickReplies.push({
-            'label': '아침',
-            'action': 'message',
-            'messageText': '금요일 아침'
-        });
-        quickReplies.push({
-            'label': '점심',
-            'action': 'message',
-            'messageText': '금요일 점심'
-        });
-        quickReplies.push({
-            'label': '저녁',
-            'action': 'message',
-            'messageText': '금요일 저녁'
-        });
-    }
-    if (question == "금요일 아침") {
-        responseText = data["friday"]["breakfast"];
-    }
-    if (question == "금요일 점심") {
-        responseText = data["friday"]["lunch"];
-    }
-    if (question == "금요일 저녁") {
-        responseText = data["friday"]["dinner"];
+    else {
+        responseText = "잘못된 내용입니다."
     }
 
     const responseBody = {
